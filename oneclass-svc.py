7c7922a8f79f10049e07ef0791d60e09cac264d6
Ivan Ivani <34777752+realivanivani@users.noreply.github.com>
2018-01-03 08:58:50 +0100

    still in progress

--- oneclass-svc.py
+++ oneclass-svc.py
@@ -3,75 +3,136 @@
 
 import pandas as pd
 import numpy as np
+import os
 
 
-def cvs_process(csv_file, target_value):
-    # Function to clean cvs from NaN,
-    # transform features into labels using LabelEncoder
-    # and select features by choosing those which differ
-    #
-    # It returns transformed dataframe object and target list with target_values
-    #
-    from sklearn.preprocessing import LabelEncoder
+path = '/Users/ivanivani/Documents/python/net_traffic_classifier/net-traffic-classifier'
 
-    # change NaN into integer
-    data=csv_file.fillna('na')
+csv_file_train = pd.read_csv(os.path.join(path,'reqnorm-train.csv'), dtype=str)
+csv_file_test = pd.read_csv(os.path.join(path,'reqnorm-test.csv'), dtype=str)
+csv_file_outliers = pd.read_csv(os.path.join(path,'reqanom-test.csv'), dtype=str)
 
-    class_le = LabelEncoder()
-    size=data.shape
-    translbl = pd.DataFrame()
-    feature_list = []
+size_train=np.size(csv_file_train,0)
+size_test=np.size(csv_file_test,0)
+size_outlier=np.size(csv_file_outliers,0)
 
-    for i in range(0, size[1]):
-        translbl = class_le.fit_transform(data.values[:,i].astype(str))
-        data.values[:,i]=translbl
-        if not translbl.sum() == 0:
-            feature_list.insert(i,i)
-
-    return(data,[target_value for col in range(size[0])], feature_list)
-
-csv_file_train = pd.read_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-train.csv', dtype=str)
-csv_file_test1 = pd.read_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-test.csv', dtype=str)
-csv_file_test2 = pd.read_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqanom-test.csv', dtype=str)
-
-[data_train,target_train, feature_list] = cvs_process(csv_file_train,0)
-[data_test1,target_test1] = cvs_process(csv_file_test1,0)
-[data_test2,target_test2] = cvs_process(csv_file_test2,1)
-
-data_test = pd.DataFrame(pd.concat([data_test1,data_test2], ignore_index=True))
-target_test = pd.DataFrame(pd.concat([target_test1,target_test2], ignore_index=True))
+data = pd.DataFrame()
+data = pd.concat([csv_file_train,csv_file_test], ignore_index=True)
+data = pd.concat([data,csv_file_outliers], ignore_index=True)
 
 
-## Model selection using Grid Search with OneClassSVM model
-##
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA
+# change NaN into string
+data=data.fillna('na')
+
+size=data.shape
+relevant_features = []
+
+for i in range(0, size[1]):
+    uniques = data.apply(lambda x: x.nunique())
+    if  uniques[i] != 1:
+        relevant_features.insert(i,str(i))
+new_data = data[relevant_features].apply(lambda x: ' '.join(x),axis=1)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+new_data=vectorizer.fit_transform(new_data.values)
+print(new_data.shape)
+
+data_train = new_data[:size_train,:]
+data_test = new_data[size_train:size_test,:]
+data_outliers = new_data[size_test:,:]
+
+
+print('Done importing cvs and Tfidf vectorizing')
+
+print('Relevant features:', relevant_features)
+
+## Using simple OneClassSVM model
+## example taken from scikit.learn webpage
+
+# fit the model
 from sklearn.svm import OneClassSVM
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from pprint import pprint
+from time import time
+import logging
+from sklearn.metrics import make_scorer
 
-pipe_svc = Pipeline([('scl', StandardScaler()),
-                    ('pca', PCA(n_components=2)),
-            ('clf', OneClassSVM(random_state=1))])
+print('\nStart fitting..')
 
-param_grid = [{'clf__nu': np.linspace(0.01, 0.99, 99),
-               'clf__kernel': ['linear']},
-                 {'clf__nu': np.linspace(0.01, 0.99, 99),
-                  'clf__gamma': np.logspace(-9, 3, 13),
-                  'clf__kernel': ['rbf']}]
+# # from scikit-learn webpage, Sample pipeline for text feature extraction and evaluation
+# pipeline = Pipeline([
+#     ('vect', CountVectorizer()),
+#     ('tfidf', TfidfTransformer()),
+#     ('clf', OneClassSVM()),
+# ])
+#
+# param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+#
+# parameters = [{'clf__C': param_range,
+#                'clf__kernel': ['linear']},
+#                  {'clf__C': param_range,
+#                   'clf__gamma': param_range,
+#                   'clf__kernel': ['rbf']}]
+#
+# scorer = make_scorer(roc_auc_score, pos_label=0)         # defining scorer
+#
+# # find the best parameters for both the feature extraction and the
+# # classifier
+# grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1, scoring=scorer)
+#
+# print("Performing grid search...")
+# print("pipeline:", [name for name, _ in pipeline.steps])
+# print("parameters:")
+# print(parameters)
+# t0 = time()
+# grid_search.fit(data_train)
+# print("done in %0.3fs" % (time() - t0))
+# print()
+#
+# print("Best score: %0.3f" % grid_search.best_score_)
+# print("Best parameters set:")
+# best_parameters = grid_search.best_estimator_.get_params()
+# for param_name in sorted(parameters.keys()):
+#         print("\t%s: %r" % (param_name, best_parameters[param_name]))
+#
 
-gs = GridSearchCV(estimator=pipe_svc,
-                  param_grid=param_grid,
-                  scoring='accuracy',
-                  cv=10,
-                  n_jobs=-1)
-gs = gs.fit(data_train.iloc[:,feature_list], target_train)
+clf = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+clf.fit(data_train)
 
-print(gs.best_score_)       # prints the score of the optimal hyperparameters
-print(gs.best_params_)      # prints the hyperparameters
+print('Done fitting\n\nTesting..')
+y_pred_train = clf.predict(data_train)
+y_pred_test = clf.predict(data_test)
+y_pred_outliers = clf.predict(data_outliers)
+n_corr_train = y_pred_train[y_pred_train == 1].size
+#print(n_corr_train)
+n_corr_test = y_pred_test[y_pred_test == 1].size
+#print(n_corr_test)
+n_corr_outliers = y_pred_outliers[y_pred_outliers == -1].size
+#print(n_corr_outliers)
+n_error_train = y_pred_train[y_pred_train == -1].size
+n_error_test = y_pred_test[y_pred_test == -1].size
+n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
+print(n_error_test)
 
+## Performance metrics
+print('\nPrecision metrics')
 
-# Applying the hyperparameters to the test set
-clf = gs.best_estimator_
-clf.fit(data_train.iloc[:,feature_list], target_train)           # fitting
-print('Test accuracy: %.3f' % clf.score(data_test.iloc[:,feature_list], target_test))    #score for the testset
+prec = (n_corr_test / (n_corr_test + n_error_outliers))         # precision
+recall = (n_corr_test / (n_error_outliers + n_corr_test))       # recall
+f1_score = 2*(prec*recall/(prec+recall))                        # f1 score
+
+positive_detection = (n_corr_outliers / size_outlier)           # TP / TP + FN
+false_detection = (n_error_test / size_test)                    # FP / FP + TN
+
+# using my own definitions
+print('\nPrecision: %.3f' % prec)
+print('\nRecall: %.3f' % recall)
+print('\nF1: %.3f' % f1_score)
+
+# ROC AUC (Positive_detection / false_detection)
+print('\nPositive detection: %.3f' % positive_detection)
+print('\nFalse detection: %.3f' % false_detection)
--- text-process.py
+++ text-process.py
@@ -1,5 +1,14 @@
 import numpy as np
 import pandas as pd
+import pyprind
+import os
+
+def file_len(textfile):
+    # checking the number of lines in the file
+    with open(textfile) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
 def http_parser(textfile):
     ## Takes a text file with HTTP requests and parses all attributes as features
@@ -9,51 +18,70 @@ def http_parser(textfile):
 
     reqget = pd.DataFrame()             # GET requests
     reqpost = pd.DataFrame()            # POST requests
+    reqall = pd.DataFrame()             # All requests
     singlereq = pd.DataFrame()          # intermediate DataFrame object
 
+    # defining the status bar of the import
+    num_of_lines = file_len(textfile)
+    pbar = pyprind.ProgBar(num_of_lines)       # status bar
+    # this is not much useful, it's more for the estetics as I like status bars
+
+    # import
     with open(textfile) as infile:
         for line in infile:
-                if not len(line.strip()) == 0:
+                if len(line.strip()) != 0:
                     splitline = pd.DataFrame(line.split())
                     singlereq= pd.concat([singlereq,splitline], ignore_index=True)
 
-                elif not len(singlereq.index) == 0:
-                    if singlereq.values[0,0] == 'POST':
+                elif len(singlereq.index) != 0:
+                    if singlereq.values[0,0] != 'GET':
                         splitline = pd.DataFrame(next(infile).split())
-                        singlereq = pd.concat([singlereq,splitline], ignore_index=True)
+                        singlereq = pd.concat([singlereq,splitline], ignore_index=True)       # this had no value
                         singlereq = singlereq.T
                         reqpost = pd.concat([reqpost,singlereq], ignore_index=True)
                     else:
+                        # alligning columns of GET to be as POST and PUT
+                        singlereq.loc[34,0] = singlereq.values[32,0]
+                        singlereq.loc[35,0] = singlereq.values[33,0]
+                        singlereq.loc[32,0] = 'Content-Type:'
+                        singlereq.loc[33,0] = ''
+                        singlereq.loc[36,0] = 'Content-Length:'
+                        singlereq.loc[37,0] = ''
                         singlereq = singlereq.T
                         reqget = pd.concat([reqget,singlereq], ignore_index=True)
 
+                    reqall = pd.concat([reqall,singlereq], ignore_index=True)
                     del singlereq
                     singlereq = pd.DataFrame()
-                else:
-                    next(infile)
 
-    return (reqget,reqpost)
+                pbar.update()
 
-# Extracting objects from HTTP requests as features and putting it into DataFrame
-normaltraffic_train = '/Users/ivanivani/Documents/python/net_traffic_classifier/normalTrafficTraining.txt'
-anomaloustraffic_test = '/Users/ivanivani/Documents/python/net_traffic_classifier/anomalousTrafficTest.txt'
-normaltraffic_test = '/Users/ivanivani/Documents/python/net_traffic_classifier/normalTrafficTest.txt'
+    return (reqall, reqget,reqpost)
 
-[reqgetnorm_train,reqpostnorm_train] = http_parser(normaltraffic_train)
-reqnorm_train=pd.DataFrame(pd.concat([reqgetnorm_train,reqpostnorm_train], ignore_index=True))
-# Saving to .csv for easier handdling
-reqnorm_train.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-train.csv', index=False)
-reqgetnorm_train.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-get-train.csv', index=False)
-reqpostnorm_train.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-post-train.csv', index=False)
+## Import
+# Extracting objects from HTTP requests as features into DataFrames and saving .csv
 
-[reqgetanom,reqpostanom] = http_parser(anomaloustraffic_test)
-reqanom_test=pd.DataFrame(pd.concat([reqgetanom,reqpostanom], ignore_index=True))
-reqanom_test.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqanom-test.csv', index=False)
-reqgetanom.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqanom-get-test.csv', index=False)
-reqpostanom.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqanom-post-test.csv', index=False)
+path = '/Users/ivanivani/Documents/python/net_traffic_classifier'
 
-[reqgetnorm_test,reqpostnorm_test] = http_parser(normaltraffic_test)
-reqnorm_test=pd.DataFrame(pd.concat([reqgetnorm_test,reqpostnorm_test], ignore_index=True))
-reqnorm_test.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-test.csv', index=False)
-reqgetnorm_test.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-get-test.csv', index=False)
-reqpostnorm_test.to_csv('/Users/ivanivani/Documents/python/net_traffic_classifier/reqnorm-post-test.csv', index=False)
+normaltraffic_train = 'normalTrafficTraining.txt'
+anomaloustraffic_test = 'anomalousTrafficTest.txt'
+normaltraffic_test = 'normalTrafficTest.txt'
+
+#Saving to .csv for easier handdling
+print ('\nImporting ' + normaltraffic_train + ' file')
+[reqnorm_train, reqgetnorm_train,reqpostnorm_train] = http_parser(os.path.join(path, normaltraffic_train))
+reqnorm_train.to_csv(os.path.join(path,'net-traffic-classifier/reqnorm-train.csv'), index=False)
+reqgetnorm_train.to_csv(os.path.join(path,'net-traffic-classifier/reqnorm-get-train.csv'), index=False)
+reqpostnorm_train.to_csv(os.path.join(path,'net-traffic-classifier/reqnorm-post-train.csv'), index=False)
+
+print ('\nImporting ' + anomaloustraffic_test + ' file')
+[reqanom_test,reqgetanom,reqpostanom] = http_parser(os.path.join(path, anomaloustraffic_test))
+reqanom_test.to_csv(os.path.join(path,'net-traffic-classifier/reqanom-test.csv'), index=False)
+reqgetanom.to_csv(os.path.join(path,'net-traffic-classifier/reqanom-get-test.csv'), index=False)
+reqpostanom.to_csv(os.path.join(path,'net-traffic-classifier/reqanom-post-test.csv'), index=False)
+
+print ('\nImporting ' + normaltraffic_test + ' file')
+[reqnorm_test, reqgetnorm_test,reqpostnorm_test] = http_parser(os.path.join(path, normaltraffic_test))
+reqnorm_test.to_csv(os.path.join(path,'net-traffic-classifier/reqnorm-test.csv'), index=False)
+reqgetnorm_test.to_csv(os.path.join(path,'net-traffic-classifier/reqnorm-get-test.csv'), index=False)
+reqpostnorm_test.to_csv(os.path.join(path,'net-traffic-classifier/reqnorm-post-test.csv'), index=False)
